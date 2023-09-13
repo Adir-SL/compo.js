ad0250baaa0c937ac30c57f426373ef50523dad0
Adir <slutzkis@gmail.com>
2023-09-13 11:39:18 +0300

    Update compo.js

--- compo.js
+++ compo.js
@@ -19,7 +19,7 @@ function findTags(){
     var i;
     for (i = 0; i < x.length; i++) {
         if(x[i].tagName.includes("-") == true){
-            console.log(x[i].outerHTML);
+            console.log(x[i].tagName);
         }
     }
 }
