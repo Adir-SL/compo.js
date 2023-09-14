eaac76ed315348e70d345655a747988e960fe151
Adir <slutzkis@gmail.com>
2023-09-14 11:55:46 +0300

    Update compo.js

--- compo.js
+++ compo.js
@@ -19,7 +19,6 @@ function findTags(){
     var i;
     for (i = 0; i < x.length; i++) {
         if(x[i].tagName.includes("-") == true){
-            // console.log(x[i].tagName);
             const box = x[i];
 
             fetch(x[i].tagName + '.html')
