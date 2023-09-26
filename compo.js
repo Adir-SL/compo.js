3d2aa820a2e569c044ca0c5ddb7fcfda0e14aeb5
Adir <slutzkis@gmail.com>
2023-09-26 11:16:50 +0300

    Update compo.js

--- compo.js
+++ compo.js
@@ -1,19 +1,3 @@
-/*
-function loadPage() {
-    const box = document.getElementById('box');
-
-    fetch('hero.html')
-        .then(response => response.text())
-        .then(html => {
-        box.innerHTML = html;
-        })
-        .catch(error => {
-        console.log(error);
-        });
-    }
-    loadPage();
-*/
-
 function findTags(){
     // var x = document.body.children;
     var x = document.querySelectorAll("*");
